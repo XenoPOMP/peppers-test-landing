3f9295ce9485cd16ce193f2d6207f93d425ad50d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-10-26 00:27:59 +0300

    сделал футер

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,16 +1,50 @@
 import cn from 'classnames';
+import Image from 'next/image';
+import Link from 'next/link';
 import { FC } from 'react';
 
+import chatBubbleIcon from '@/public/images/icons/Comment_bubble.png';
+import Button from '@/src/components/ui/Button/Button';
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 import UiGrid from '@/src/components/ui/UiGrid/UiGrid';
+import { FuturaBookPT } from '@/src/fonts/futura-font-familiy';
+import bgImage from '@/task/design/images/bg.jpg';
 
 import styles from './Footer.module.scss';
 import type { FooterProps } from './Footer.props';
 
 const Footer: FC<FooterProps> = ({}) => {
   return (
-    <footer className={cn(styles.appFooter)}>
-      <UiContainer>Footer</UiContainer>
+    <footer className={cn(styles.appFooter, FuturaBookPT.className)}>
+      <UiContainer maxWidth={'1300px'} className={cn(styles.body)}>
+        <nav className={cn(styles.links)}>
+          <h3>2016 © ShopGuide</h3>
+
+          <ul>
+            <li>
+              <Link href={'/'}>Политика конфиденциальности</Link>
+            </li>
+
+            <li>
+              <Link href={'/'}>Правила пользования</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Link href={'/'}>
+          <Button
+            variant={'transparent'}
+            className={cn(
+              'text-[.8em] uppercase flex gap-[.75em] items-center'
+            )}
+          >
+            <Image src={chatBubbleIcon} alt={'Chat bubble icon'} />
+            Связаться с нами
+          </Button>
+        </Link>
+      </UiContainer>
+
+      <Image src={bgImage} alt={'Background'} className={styles.background} />
     </footer>
   );
 };
