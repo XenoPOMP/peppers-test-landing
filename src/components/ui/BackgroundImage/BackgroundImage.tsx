b412c9f39b11674d094602ec371f76c180c3da73
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-10-26 23:40:15 +0300

    добавил spread оператор для style

--- src/components/ui/BackgroundImage/BackgroundImage.tsx
+++ src/components/ui/BackgroundImage/BackgroundImage.tsx
@@ -8,12 +8,14 @@ import type { BackgroundImageProps } from './BackgroundImage.props';
 const BackgroundImage: FC<BackgroundImageProps> = ({
   className,
   objectFit = 'cover',
+  style,
   ...props
 }) => {
   return (
     <Image
       className={cn(styles.background, 'select-none', className)}
       style={{
+        ...style,
         objectFit,
       }}
       {...props}
